1087656ddc6bbd27d0679ab4f0c893ddc43bdf89
Antika Noor <antika.noor@bjitacademy.com>
2023-08-29 16:15:28 +0600

    express js structured code added with .env

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const mangaRouter = require('./routes/manga')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({ extended: true }))
+
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).send({ message: "Invalid JSON syntax!" })
+  }
+  next()
+})
 
 app.use("/manga", mangaRouter)
 
@@ -22,5 +33,6 @@ app.route('*')
   })
 
 app.listen(8000, () => {
+  console.log(process.env.TEST_DB)
   console.log("Server is running on 8000...")
 })
